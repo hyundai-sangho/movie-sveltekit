58f79760b3ac1b08f7d884fb673e473e2191b45d
chosangho <hyundai_sangho@naver.com>
2022-05-20 01:52:20 +0900

    svelte.config.js 수정

--- svelte.config.js
+++ svelte.config.js
@@ -2,9 +2,11 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+
+    target: '#svelte'
+  }
 };
 
 export default config;
